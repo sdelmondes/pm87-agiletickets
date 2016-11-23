fdba4648e8e4dc91cdd6a857ef1bd1145d675a03
Sabrina <sabrinadelmondes@hotmail.com>
2016-11-23 17:18:48 -0200

    t

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -3,7 +3,6 @@ package br.com.caelum.agiletickets.domain.precos;
 import java.math.BigDecimal;
 
 import br.com.caelum.agiletickets.models.Sessao;
-import br.com.caelum.agiletickets.models.TipoDeEspetaculo;
 
 public class CalculadoraDePrecos {
 
