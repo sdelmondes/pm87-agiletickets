4f4a4a19316662cf38f2435dcb00378ee34a42ee
Sabrina <sabrinadelmondes@hotmail.com>
2016-11-23 17:13:03 -0200

    Alterações de refatoração

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -8,8 +8,29 @@ import br.com.caelum.agiletickets.models.TipoDeEspetaculo;
 public class CalculadoraDePrecos {
 
 	public static BigDecimal calcula(Sessao sessao, Integer quantidade) {
-		BigDecimal preco;
 		
+		BigDecimal preco= sessao.getPreco();
+		
+		switch (sessao.getEspetaculo().getTipo()) {
+		case CINEMA:
+		case SHOW:
+			preco = calculaPrecoPorLotacao(preco, sessao, 0.05, 0.10);
+			break;
+		case BALLET:
+			preco = calculaPrecoPorLotacao(preco,sessao, 0.50, 0.20);
+			preco = calculaPrecoPorDuracao(preco,sessao, 60, 0.10);
+			break;
+		case ORQUESTRA:
+			preco = calculaPrecoPorLotacao(preco,sessao, 0.50, 0.20);
+			preco = calculaPrecoPorDuracao(preco,sessao, 60, 0.10);
+			break;
+		default:
+			preco = sessao.getPreco();
+			break;
+		}
+		
+	
+	/*
 		if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.CINEMA) || sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.SHOW)) {
 			//quando estiver acabando os ingressos... 
 			if((sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue() <= 0.05) { 
@@ -41,8 +62,25 @@ public class CalculadoraDePrecos {
 			//nao aplica aumento para teatro (quem vai é pobretão)
 			preco = sessao.getPreco();
 		} 
-
+*/
 		return preco.multiply(BigDecimal.valueOf(quantidade));
+	}
+	
+	public static BigDecimal calculaPrecoPorLotacao (BigDecimal preco, Sessao sessao, double fatorLotacao, double fatorEncarecedor){
+		if((sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue() <= fatorLotacao){
+			preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(fatorEncarecedor)));
+		} else {
+			preco = sessao.getPreco();
+		}
+		return preco;
+	}
+	
+	public static BigDecimal calculaPrecoPorDuracao (BigDecimal preco, Sessao sessao, int minutos, double fatorEncarecedor){
+		
+		if(sessao.getDuracaoEmMinutos() > minutos){
+			preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(fatorEncarecedor)));
+		}
+		return preco;
 	}
 
 }
