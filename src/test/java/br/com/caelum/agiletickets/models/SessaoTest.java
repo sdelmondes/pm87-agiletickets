0cb2ddbcc213536eddb20887bc01b582a85ce87a
Sabrina <sabrinadelmondes@hotmail.com>
2016-11-23 14:57:12 -0200

    removido teste duplicado

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -6,11 +6,11 @@ import org.junit.Test;
 public class SessaoTest {
 
 	@Test
-	public void deveVender1ingressoSeHa1vagas() throws Exception {
+	public void deveVender2IngressosSeHa2vagas() throws Exception {
 		Sessao sessao = new Sessao();
-        sessao.setTotalIngressos(1);
+        sessao.setTotalIngressos(2);
 
-        Assert.assertTrue(sessao.podeReservar(1));
+        Assert.assertTrue(sessao.podeReservar(2));
 	}	
 	
 	@Test
@@ -21,13 +21,6 @@ public class SessaoTest {
         Assert.assertTrue(sessao.podeReservar(1));
 	}
 	
-	@Test
-	public void deveVender5ingressosSeHa10vagas() throws Exception {
-		Sessao sessao = new Sessao();
-		sessao.setTotalIngressos(10);
-		
-		Assert.assertTrue(sessao.podeReservar(5));
-	}
 
 	@Test
 	public void naoDeveVender3ingressoSeHa2vagas() throws Exception {
